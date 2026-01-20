3697c0cc5efb37a6fe0ce97f17888c10bb964967
Andy Durant <andy@ajdurant.co.uk>
2026-01-20 10:51:53 +0000

    auto-update sqlitespy to 1.9.32

--- sqlitespy/tools/chocolateyUninstall.ps1
+++ sqlitespy/tools/chocolateyUninstall.ps1
@@ -7,6 +7,6 @@ $pkgPath = Split-Path -Parent -Path $toolsdir
     Remove-Item -Path (Join-Path -Path $_ -ChildPath 'SQLiteSpy.lnk') -ErrorAction SilentlyContinue
 }
 
-# get the name of the file created by Install-ChocolateyZipPackage
+# get the name of the file created by Get-ChocolateyUnzip
 $zipFilename = (Get-Item -Path (Join-Path -Path $pkgPath -ChildPath '*.zip.txt') | Select-Object -First 1).Basename
 Uninstall-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -ZipFileName $zipFilename
